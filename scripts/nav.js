73684017d943a102ae71c4fe6234fb140c30e7a8
JuanCamiloBM <jcbetanm@gmail.com>
2022-05-27 12:40:42 -0500

    Portafolio 2.0

--- scripts/nav.js
+++
@@ -1,6 +0,0 @@
-const ham = document.querySelector('.ham');
-const links = document.querySelector('.links-menu');
-
-ham.addEventListener('click', () => {
-    links.classList.toggle('active'); //cambiar display de none a flex según los estilos.
-})
